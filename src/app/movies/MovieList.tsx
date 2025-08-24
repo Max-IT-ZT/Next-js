4072caf93cf3bd0fe21e207995bdcff0031dceea
Max-IT-ZT <babuskomax@gmail.com>
2025-08-24 22:01:50 +0300

    add details

--- src/app/movies/MovieList.tsx
+++ src/app/movies/MovieList.tsx
@@ -3,6 +3,7 @@ import { getTrendingMovies, Movie } from "@/app/api/tmdb";
 import Image from "next/image";
 import { FaStar } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function MoviesList() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -35,27 +36,26 @@ export default function MoviesList() {
 
       <ul className="flex gap-4 overflow-x-scroll scrollbar-hide mb-8 py-4 px-2">
         {movies.map((movie) => (
-          <li
-            key={movie.id}
-            className="flex-shrink-0 p-3 border border-white/20 backdrop-blur-md shadow-lg text-white rounded-lg hover:scale-105 transition-transform duration-300"
-          >
-            <div className="w-[100px] h-[150px] sm:w-[150px] sm:h-[225px] relative flex flex-col hover:scale-105 transition-transform duration-300 overflow-x-scroll scrollbar-hide">
-              <Image
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt={movie.title}
-                fill
-                sizes="(max-width: 640px) 100px, (max-width: 768px) 150px, 150px"
-                className="object-cover rounded-lg"
-              />
-            </div>
-            <div className="flex text-center flex-col gap-1 justify-between mt-2 text-sm">
-              <p className="flex items-center justify-center gap-1 font-bold">
-                <FaStar className="text-red-500" />
-                {movie.vote_average.toFixed(1)}
-              </p>
-              <p>{movie.release_date}</p>
-            </div>
-          </li>
+          <Link key={movie.id} href={`/movies/${movie.id}`}>
+            <li className="flex-shrink-0 p-3 border border-white/20 backdrop-blur-md shadow-lg text-white rounded-lg hover:scale-105 transition-transform duration-300">
+              <div className="w-[100px] h-[150px] sm:w-[150px] sm:h-[225px] relative flex flex-col hover:scale-105 transition-transform duration-300 overflow-x-scroll scrollbar-hide">
+                <Image
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  alt={movie.title}
+                  fill
+                  sizes="(max-width: 640px) 100px, (max-width: 768px) 150px, 150px"
+                  className="object-cover rounded-lg"
+                />
+              </div>
+              <div className="flex text-center flex-col gap-1 justify-between mt-2 text-sm">
+                <p className="flex items-center justify-center gap-1 font-bold">
+                  <FaStar className="text-red-500" />
+                  {movie.vote_average.toFixed(1)}
+                </p>
+                <p>{movie.release_date}</p>
+              </div>
+            </li>
+          </Link>
         ))}
         {page < (totalPages ?? 1) && (
           <div className="flex justify-center">
