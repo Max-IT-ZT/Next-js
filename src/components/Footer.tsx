0640b3f87964cb6b0847645211376b2b50292415
Max-IT-ZT <babuskomax@gmail.com>
2025-08-31 02:36:37 +0300

    fix-logo3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
             rel="noopener noreferrer"
           >
             <Image
-              src="/img/logoMaxDev.webp"
+              src="/img/logo.webp"
               priority
               alt="Logo"
               width={60}
